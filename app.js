77fef255f9549799ae350d11472caaee7fec04f4
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-02-05 16:52:05 +0900

    version update 0.2.1

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 const express = require('express')
+const myCommon = require('dhkdtld37-common-fun2')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-	let myJson = { "Hello" : "World!" }
-	res.json(myjSon)
+  const a = 1
+  const b = 10
+  let myJson = { "Hello": "World", "ADD": myCommon.add(a, b) }
+
+  res.json(myJson)
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
-
-const router = express.Router();
-
-router.get('/artists/:id', function(req, res) {
-        console.log("id는 " + req.params.id + " 입니다.")
-        res.send("id : " + req.params.id)
-});
-
-router.get('/artists/:id/company/:company', function(req, res) {
-        res.send("id : " + req.params.id + " 회사 : " + req.params.company)
-});
